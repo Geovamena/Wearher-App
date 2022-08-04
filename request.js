27ed71c1a8919234174193b88d287669c6be462d
Geovanny Mena <geovannymenavc@gmail.com>
2022-08-03 19:25:26 -0500

    second commit

--- request.js
+++ request.js
@@ -3,13 +3,15 @@ const key = `65ae5e6a8b671e9287a036cf4edca090
 
 
 
-// const baseURL = 'http://api.lopenweathermap.org/data/2.5/weather?q=Lagos&appid=cbe3dd267a18f6c89943b3eff94f1ed7';
+const baseURL = `http://api.lopenweathermap.org/data/2.5/weather?q=Lagos&appid=65ae5e6a8b671e9287a036cf4edca090
+`;
 
-// fetch(baseURL)
-//     .then((data) => { console.log('response', data.json()) })
-//     .catch((error) => {
-//         console.log(error);
-//     });
+fetch(baseURL)
+    .then((data) => { console.log('response', data.json()) })
+    .catch((error) => {
+        console.log(error);
+    });
+
 
 const requestCity = async (city) => {
     const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
